20c116da352c1eaeeae7ec82b4a5baed345f13b5
tarheel300 <andypguice@gmail.com>
2024-08-23 09:01:05 -0400

    Integrating Bootstrap And Removing Initial Files

---
+++ src/Students.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Student from './Student.js';
+import {students} from './constants.js'
+
+class Students extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {visibleStudents: students.map((_, index) => index === 0)} //Make first element true, and the rest false
+    }
+
+    toggleVisibility = (index) => {
+        this.setState((prevState) => {
+            const newVisibility = prevState.visibleStudents.map((isVisible, i) => 
+                i === index ? true : false
+            );
+            return {visibleStudents: newVisibility};
+        })
+    }
+
+    render() {
+        const bootstrapColumns = Math.floor(12 / this.state.visibleStudents.length)
+
+        return (
+            <div className="container students">
+                <div className="row buttons">
+                    {students.map((item, index) => (
+                        <button key={`buttonStudent${index}`}
+                            className={`col-lg-${bootstrapColumns} col-md-${bootstrapColumns} col-sm-${bootstrapColumns} col-${bootstrapColumns}`}
+                            onClick={() => this.toggleVisibility(index)}>{item}</button>
+                    ))}
+                </div>
+                <div className="col-lg-12 col-md-12 col-sm-12 col-12">
+                    {students.map((item, index) => (
+                        <Student key={`tab${item}`} studentName={item} isVisible={this.state.visibleStudents[index]}/>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Students;
--- src/Two.js
+++
@@ -1,5 +0,0 @@
-function Two() {
-    return <p>And this, by Jove!</p>
-}
-
-export default Two;
