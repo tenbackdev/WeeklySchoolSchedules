431878fa98cf8c70fdc428e3ce0bb79e566f44e9
tarheel300 <andypguice@gmail.com>
2024-09-11 08:55:15 -0400

    Transitioning to SubComponent

--- src/StudentPlanner.js
+++ src/StudentPlanner.js
@@ -1,11 +1,7 @@
 import React from 'react'
+import DayMeal from './DayMeal.js'
 
-export default function StudentPlanner({isVisible, studentName, days, meals, onClickMethod}) {
-    return (
-        <div key={`student${studentName}`} className={`student ${isVisible ? 'student-visible' : 'student-nonvisible'}`}>
-            <p>{studentName} is my name, come straight from my daddy's mane, my whole body's made of glitter, and I'll throw it in your face!</p>
-            <h3 className='meal-header'>Breakfast</h3>
-            {Array.from(days).map(([day, value]) => (
+/*
                         <div key={`breakfast${day}${studentName}`} className={`student-meal container ${!value ? 'day-meal-disabled': 'day-meal-enabled'}`}>
                             <p className='test'>{day}</p>
                             <button className={`meal-home ${meals.get(day).breakfast === 'Home' ? 'meal-active' : 'meal-inactive'}`}
@@ -13,16 +9,33 @@ export default function StudentPlanner({isVisible, studentName, days, meals, onC
                             <button className={`meal-school ${meals.get(day).breakfast === 'School' ? 'meal-active' : 'meal-inactive'}`}
                                 onClick={() => onClickMethod(studentName, day, 'breakfast', 'School')}>School</button>
                         </div>
+*/
+
+export default function StudentPlanner({isVisible, studentName, days, meals, onClickMethod}) {
+    return (
+        <div key={`student${studentName}`} className={`student ${isVisible ? 'student-visible' : 'student-nonvisible'}`}>
+            <p>{studentName} is my name, come straight from my daddy's mane, my whole body's made of glitter, and I'll throw it in your face!</p>
+            <h3 className='meal-header'>Breakfast</h3>
+            {Array.from(days).map(([day, value]) => (
+                        <DayMeal key={`breakfast${day}${studentName}`}
+                            isEnabled={!value}
+                            studentName={studentName}
+                            day={day}
+                            meals={meals}
+                            meal='breakfast'
+                            onClickMethod={onClickMethod}/>
+            
             ))}
             <h3 className='meal-header'>Lunch</h3>
             {Array.from(days).map(([day, value]) => (
-                        <div key={`lunch${day}${studentName}`} className={`student-meal container ${!value ? 'day-meal-disabled': 'day-meal-enabled'}`}>
-                            <p className='test'>{day}</p>
-                            <button className={`meal-home ${meals.get(day).lunch === 'Home' ? 'meal-active' : 'meal-inactive'}`}
-                                onClick={() => onClickMethod(studentName, day, 'lunch', 'Home')}>Home</button>
-                            <button className={`meal-school ${meals.get(day).lunch === 'School' ? 'meal-active' : 'meal-inactive'}`}
-                                onClick={() => onClickMethod(studentName, day, 'lunch', 'School')}>School</button>
-                        </div>
+                        <DayMeal key={`lunch${day}${studentName}`}
+                            isEnabled={!value}
+                            studentName={studentName}
+                            day={day}
+                            meals={meals}
+                            meal='lunch'
+                            onClickMethod={onClickMethod}/>
+            
             ))}
         </div>
     )
